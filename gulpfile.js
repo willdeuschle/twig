6007d03bb6686f922c0b17ee0aded7a3c4dcf2e5
willdeuschle <wjdeuschle@gmail.com>
2016-10-22 20:47:28 -0400

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use-strict';
+
+var gulp = require('gulp');
+var exec = require('child_process').exec;
+var gutil = require('gulp-util');
+
+
+var cmd = 'elm-make src/App.elm --output dist/elm-water.js';
+
+gulp.task('default', ['elm']);
+
+gulp.task('elm', function(cb) {
+  exec(cmd, function(err, stdout, stderr) {
+      if (err) {
+        gutil.log(gutil.colors.red('elm make: '), gutil.colors.red(stderr));
+      } else {
+        gutil.log(gutil.colors.green('elm make: '), gutil.colors.green(stdout));
+      }
+    });
+    cb();
+  }
+);
